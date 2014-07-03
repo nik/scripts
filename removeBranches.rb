5bc2b1f8d8ddd715319276f02e6379d8fd64367c
Nik Kovurov <nickkov89@gmail.com>
2014-07-03 13:33:58 -0400

    dont use grb

--- removeBranches.rb
+++ removeBranches.rb
@@ -1,6 +1,7 @@
 branches = IO.popen('git branch')
 for branch in branches.readlines
   if branch.strip.match(/^#{ARGV[0]}/)
-    system("grb rm #{branch.strip}")
+    system("git push origin :#{branch.strip}")
+    system("git branch -D #{branch.strip}")
   end
 end
